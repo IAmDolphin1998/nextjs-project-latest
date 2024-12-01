c8cb96b5b78c68b0a50f4ba64dc676982035322a
IAmDolphin1998 <giuseppe.defina1998@libero.it>
2024-12-01 13:52:57 +0100

    Update routing management on search page

--- src/components/SearchInterface/index.tsx
+++ src/components/SearchInterface/index.tsx
@@ -24,64 +24,6 @@ export default function SearchInterface() {
               ? `UniPaper - Results for: ${indexUiState.query}`
               : 'UniPaper - Results page';
           },
-          createURL({ qsModule, routeState, location }) {
-            const indexUiState = routeState[indexName] || {};
-            const basePath = `${location.protocol}//${location.host}${location.pathname}`;
-
-            const queryParameters: any = {};
-            if (indexUiState.query) {
-              queryParameters.query = encodeURIComponent(indexUiState.query);
-            }
-
-            const refinementLists = indexUiState.refinementList ?? {};
-            if (refinementLists['Area.Name']) {
-              queryParameters.areas =
-                refinementLists['Area.Name'].map(encodeURIComponent);
-            }
-
-            if (refinementLists['Tags.Name']) {
-              queryParameters.tags =
-                refinementLists['Tags.Name'].map(encodeURIComponent);
-            }
-
-            if (indexUiState.page !== undefined && indexUiState.page !== 1) {
-              queryParameters.page = encodeURIComponent(indexUiState.page);
-            }
-
-            const queryString = qsModule.stringify(queryParameters, {
-              addQueryPrefix: true,
-              arrayFormat: 'comma',
-            });
-
-            return `${basePath}${queryString}`;
-          },
-          parseURL({ qsModule, location }) {
-            const queryParameters = qsModule.parse(location.search.slice(1));
-
-            const query = queryParameters.query
-              ? String(queryParameters.query)
-              : '';
-            const areas = queryParameters.areas
-              ? String(queryParameters.areas).split(',')
-              : [];
-            const tags = queryParameters.tags
-              ? String(queryParameters.tags).split(',')
-              : [];
-            const page = queryParameters.page
-              ? Number(queryParameters.page)
-              : 1;
-
-            return {
-              [indexName]: {
-                query: query,
-                page: page,
-                refinementList: {
-                  'Area.Name': areas,
-                  'Tags.Name': tags,
-                },
-              },
-            };
-          },
           cleanUrlOnDispose: false,
         },
       }}
